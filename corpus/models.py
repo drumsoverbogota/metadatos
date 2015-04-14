adbf7ca385a28b749f0bc66ad8e6de032b895c90
Sergio <sergiomanceranom@gmail.com>
2015-04-14 16:43:25 -0500

    Modificado el modelo

--- corpus/models.py
+++ corpus/models.py
@@ -2,9 +2,14 @@ from mongoengine import *
 
 class Corpus(Document):
     nombre = StringField(max_length=120, required=True)
+    descripcion = StringField(max_length=300)
 
 class Tag(Document):
     tag = StringField()
     descripcion = StringField()
     corpus = ReferenceField(Corpus)
 
+class Subtag(Document):
+    subtag = StringField()
+    descripcion = StringField()
+    tag = ReferenceField(Tag)
